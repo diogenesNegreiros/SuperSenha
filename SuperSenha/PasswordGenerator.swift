6537912afb39b09182b1d356a639f369bf53c6db
diogenesNegreiros <diogenesdesouza@yahoo.com.br>
2021-05-18 20:35:13 -0300

    Password generate class

--- SuperSenha/PasswordGenerator.swift
+++ SuperSenha/PasswordGenerator.swift
@@ -12,12 +12,56 @@ class PasswordGenerator {
     var numberOfCaracters: Int
     var useUperLetters: Bool
     var useLowerLetter: Bool
-    var usenumbers: Bool
+    var useNumbers: Bool
     var useEspecialCaracters: Bool
     
     var passwords: [String] = []
-    private let letters = ["abcdefghijlmnopqrstuvxz"]
-    private let numbers = ["0123456789"]
-    private let specialCaracters = ["!@#$%ˆ&*()_+=-~`][{}|';:/.,<>?"]
+    private let letters = "abcdefghijlmnopqrstuvxz"
+    private let numbers = "0123456789"
+    private let specialCaracters = "!@#$%ˆ&*()_+=-~`][{}|';:/.,<>?"
     
+    init(numberOfCaracters: Int, useLowerLetters: Bool, useNumbers: Bool, useUperLetters: Bool, useEspecialCaracters: Bool) {
+        
+        var numChars = min(numberOfCaracters, 16)
+        numChars = max(numChars, 1)
+        
+        self.numberOfCaracters = numChars
+        self.useUperLetters = useUperLetters
+        self.useLowerLetter = useLowerLetters
+        self.useNumbers = useNumbers
+        self.useEspecialCaracters = useEspecialCaracters
+    }
+    
+    func generate(total: Int) -> [String] {
+        passwords.removeAll()
+        var universe: String = ""
+        
+        if useLowerLetter {
+            universe += letters
+        }
+        
+        if useNumbers {
+            universe += numbers
+        }
+        
+        if useUperLetters {
+            universe += letters.uppercased()
+        }
+        
+        if useEspecialCaracters {
+            universe += specialCaracters
+        }
+        
+        let universeArray = Array(universe)
+        
+        while passwords.count < total {
+            var password = ""
+            for _ in 1...numberOfCaracters {
+                let index = Int(arc4random_uniform(UInt32(universeArray.count)))
+                password += String(universeArray[index])
+            }
+            passwords.append(password)
+        }
+        return passwords
+    }
 }
