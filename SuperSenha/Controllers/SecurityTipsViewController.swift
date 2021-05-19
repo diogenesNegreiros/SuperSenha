09d24278eb99a2145e1754703f25a4778e554402
diogenesNegreiros <diogenesdesouza@yahoo.com.br>
2021-05-18 22:57:26 -0300

    help close button

---
+++ SuperSenha/Controllers/SecurityTipsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecurityTipsViewController.swift
+//  SuperSenha
+//
+//  Created by Diogenes de Souza on 18/05/21.
+//
+
+import UIKit
+
+class SecurityTipsViewController: UIViewController {
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+
+}
